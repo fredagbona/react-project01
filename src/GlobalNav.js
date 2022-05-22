5010c18f36743d245dbd257fb9d5384f41aa1a90
fredagbona <fredagbona@gmail.com>
2022-05-23 00:04:24 +0200

    finalize global navigation settings

--- src/GlobalNav.js
+++ src/GlobalNav.js
@@ -11,11 +11,11 @@ const user = {
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
+  { name: 'Home', href: '#', current: true },
   { name: 'Team', href: '#', current: false },
   { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
+  { name: 'About', href: '#', current: false },
+  { name: 'Contact', href: '#', current: false },
 ]
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
@@ -53,7 +53,7 @@ export default function Example() {
                       />
                     </div>
                     <div className="hidden md:block">
-                      <div className="ml-10 flex items-baseline space-x-4">
+                      <div className="ml-10  flex items-baseline space-x-4">
                         {navigation.map((item) => (
                           <a
                             key={item.name}
